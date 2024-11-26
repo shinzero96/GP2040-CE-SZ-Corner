8277bf04d267a8d916a5dce3fe3118d9a76afa00
shinzero96 <136745149+shinzero96@users.noreply.github.com>
2024-11-26 16:26:17 +0700

    Update BoardConfig.h

--- configs/Sz's Corner V2/BoardConfig.h
+++ configs/Sz's Corner V2/BoardConfig.h
@@ -9,7 +9,7 @@
 #include "enums.pb.h"
 #include "class/hid/hid.h"
 
-#define BOARD_CONFIG_LABEL "SZ Corner Flatbox v2"
+#define BOARD_CONFIG_LABEL "SZ_Corner_Flatbox_v2"
 
 // Main pin mapping Configuration
 //                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
